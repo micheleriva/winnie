3e6f2cc4c4de58037a2daf2d67643c0398408cb3
Michele Riva <master@micheleriva.it>
2018-09-04 21:03:37 +0200

    removed line tracking

--- src/video-capture.h
+++ src/video-capture.h
@@ -45,14 +45,11 @@ inline void detectHumans() {
             stringstream temp;
             temp << weights[i];
 
-            putText(img, temp.str(),Point(found[i].x,found[i].y+50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255));
+            putText(img, temp.str(), Point(found[i].x, found[i].y+50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255));
 
             track.push_back(Point(found[i].x+found[i].width/2,found[i].y+found[i].height/2));
         }
 
-        for(size_t i = 1; i < track.size(); i++){
-            line(img, track[i-1], track[i], Scalar(255,255,0), 2);
-        }
 
         imshow("Detection", img);
 
